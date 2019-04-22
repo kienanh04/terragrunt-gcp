34c12895de2a507d7be6df43dd136f3018ff7c27
thanhbn <thanhbn87@gmail.com>
2019-04-22 09:20:11 +0700

    add dns outputs

--- vpc/output.tf
+++ vpc/output.tf
@@ -22,3 +22,13 @@ output "subnets_self_links" {
   value       = "${module.vpc.subnets_self_links}"
   description = "The self-links of subnets being created"
 }
+
+output "dns_private_name" {
+  value       = "${google_dns_managed_zone.private.name}"
+  description = "The name of private dns zone"
+}
+
+output "dns_public_name" {
+  value       = "${google_dns_managed_zone.public.name}"
+  description = "The name of public dns zone"
+}
